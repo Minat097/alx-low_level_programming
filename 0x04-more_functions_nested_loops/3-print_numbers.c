89660846605eba36886672e0ebfb0b0f244a7486
Minat097 <mina.said.it@gmail.com>
2022-10-20 10:43:54 -0700

    task

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers -  checks for checks for a digit (0 through 9).
+ *
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	int c;
+
+	while (a < 10)
+		_putchar(a++ + '0');
+
+
+	_putchar('\n');
+}
